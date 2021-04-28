47eb8b3d0e77369268a7aa0499e002ebe01a3a55
le dac khoan <khoanld98@gmail.com>
2021-04-28 16:26:36 +0700

    this is pass test

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,11 @@ Route::get('/first-build', function () {
 Route::get('/second-buid', function () {
     return 2;
 });
+
+Route::get('/test-true', function (){
+    return 1;
+});
+
+Route::get('/demo', function (){
+    return 1;
+});
